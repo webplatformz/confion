53c5901ad0e8dee7f54fc1e33918a9770006461e
Daniel Suter <suter_daniel@hotmail.com>
2015-06-06 20:18:13 +0200

    fix test

--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -4,7 +4,7 @@
 
 describe('my app', function() {
 
-  browser.get('index.html');
+  browser.get('home.html');
 
   it('should automatically redirect to /view1 when location hash/fragment is empty', function() {
     expect(browser.getLocationAbsUrl()).toMatch("/view1");
