a7340c526b7343034052a0436520d9cf6a8df108
Joachim Prinzbach <joachim.prinzbach@gmail.com>
2015-06-09 22:23:49 +0200

    Try heroku fix.

--- karma.conf.js
+++ karma.conf.js
@@ -1,44 +1,45 @@
-module.exports = function(config){
+module.exports = function (config) {
     var configuration = {
 
-    basePath : './',
+        basePath: './',
 
-    files : [
-      'app/bower_components/angular/angular.js',
-      'app/bower_components/angular-route/angular-route.js',
-      'app/bower_components/angular-mocks/angular-mocks.js',
-      'app/components/**/*.js',
-      'app/view*/**/*.js'
-    ],
+        files: [
+            'app/bower_components/angular/angular.js',
+            'app/bower_components/angular-route/angular-route.js',
+            'app/bower_components/angular-mocks/angular-mocks.js',
+            'app/components/**/*.js',
+            'app/view*/**/*.js'
+        ],
 
-      customLaunchers: {
-          Chrome_travis_ci: {
-              base: 'Chrome',
-              flags: ['--no-sandbox']
-          }
-      },
+        autoWatch: true,
 
-    autoWatch : true,
+        frameworks: ['jasmine'],
 
-    frameworks: ['jasmine'],
+        browsers: ['Chrome'],
 
-    browsers : ['Chrome'],
-
-    plugins : [
+        plugins: [
             'karma-chrome-launcher',
             'karma-jasmine',
             'karma-junit-reporter'
-            ],
+        ],
 
-    junitReporter : {
-      outputFile: 'test_out/unit.xml',
-      suite: 'unit'
-    }
+        junitReporter: {
+            outputFile: 'test_out/unit.xml',
+            suite: 'unit'
+        },
 
-  };
+
+        customLaunchers: {
+            Chrome_travis_ci: {
+                base: 'Chrome',
+                flags: ['--no-sandbox']
+            }
+        }
+
+    };
+    config.set(configuration);
 
     if (process.env.TRAVIS) {
-        configuration.browsers = ['Chrome_travis_ci'];
+        config.browsers = ['Chrome_travis_ci'];
     }
-    config.set(configuration);
 };
