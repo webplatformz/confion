e76e14b369230bee9cf9ca12236ebddf139127d2
joachimprinzbach <joachim.prinzbach@gmail.com>
2015-06-16 10:20:06 +0200

    Added test for clicking on session in overview.

---
+++ e2e-tests/accessors/overview.overview.accessor.js
@@ -0,0 +1,18 @@
+var OverviewPage = function() {
+
+    var sessionSelector = '#main-content ul li';
+
+    this.selectSession = function() {
+        var elements = element.all(by.repeater('message in vm.sessions'));
+        elements.get(0).$('a').click();
+    };
+
+    this.waitForOverviewView = function() {
+        var waitLoading = by.css(sessionSelector);
+        browser.wait(function() {
+            return browser.driver.isElementPresent(waitLoading);
+        }, 8000);
+    }
+};
+
+module.exports = OverviewPage;
