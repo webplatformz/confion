300cf1c6ce1145fcba5d37ca7c01e38b4c70b21c
joachimprinzbach <joachim.prinzbach@gmail.com>
2015-06-16 09:38:10 +0200

    Join from sessions on presenters and show presenter information in detail view.

--- app/session/session.controller.js
+++ app/session/session.controller.js
@@ -13,8 +13,8 @@ angular
 
     .controller('SessionController', SessionController);
 
-SessionController.$inject = ['$routeParams', '$firebaseObject'];
-function SessionController($routeParams, $firebaseObject) {
+SessionController.$inject = ['$routeParams', '$firebaseObject', '$scope'];
+function SessionController($routeParams, $firebaseObject, $scope) {
     var vm = this;
     vm.session = {};
 
@@ -22,4 +22,23 @@ function SessionController($routeParams, $firebaseObject) {
     var sessionId = $routeParams.sessionId;
     var sessionRef = new Firebase("https://confion.firebaseio.com/sessions/" + sessionId);
     vm.session = $firebaseObject(sessionRef);
+
+    vm.session.$loaded()
+        .then(function() {
+            var presenterRef = new Firebase("https://confion.firebaseio.com/presenters");
+            sessionRef.child(sessionId).once('value', function(sessionSnap) {
+                presenterRef.child(vm.session.presenter).once('value', function(presenterSnap) {
+                    $scope.$apply(function() {
+                        vm.session.presenter = presenterSnap.val();
+                    });
+
+                    console.log(vm.session.presenter.name);
+                });
+            });
+        })
+        .catch(function(err) {
+            console.error(err);
+        });
+
+
 }
